0b6e37714110db1364854b3f5c13659bcf3d00ad
Matheus Canellas <mtcanellas@gmail.com>
2026-02-05 08:41:12 -0300

    Atualização do model e menu

--- menu.ts
+++ menu.ts
@@ -1,15 +1,40 @@
-import readlinesync = require("readline-sync");
 import { colors } from "./src/util/Color";
 import { Conta } from "./src/model/Conta";
+import { Input } from "./src/util/Input";
 
 export function main() {
 
     let opcao: number;
-// instanciar objetos da classe conta
 
- const c1 = new Conta (1, 1234,"Sofia", 1 , 10000.00);
+    // Instanciar Objetos da Classe Conta
 
-        console.log("*****************************************************");
+    const c1 = new Conta(1, 1234, "Sofia", 1, 100000.00);
+
+    c1.visualizar();
+
+    // Testes do Método Sacar
+    console.log("Sacar 100,00: ", c1.sacar(100.00));
+    console.log("Sacar 200000.00: ", c1.sacar(200000.00));
+    console.log("Sacar 0.00: ", c1.sacar(0.00));
+
+    // Testes do Método Depositar
+    console.log("Depositar -10.00: ");
+    c1.depositar(-10.00);
+
+    console.log("Depositar 500.00: ");
+    c1.depositar(500.00);
+
+    c1.visualizar();
+
+    // console.log("O Titular da Conta é: ", c1.titular);
+    // console.log("O Saldo da Conta é: ", c1.saldo);
+
+  
+
+    while (true) {
+
+        console.log(colors.bg.blue, colors.fg.black,
+                    "*****************************************************");
         console.log("                                                     ");
         console.log("                BANCO DO BRAZIL COM Z                ");
         console.log("                                                     ");
@@ -23,70 +48,85 @@ export function main() {
         console.log("            6 - Sacar                                ");
         console.log("            7 - Depositar                            ");
         console.log("            8 - Transferir valores entre Contas      ");
-        console.log("            9 - Sair                                 ");
+        console.log("            0 - Sair                                 ");
         console.log("                                                     ");
         console.log("*****************************************************");
-        console.log("                                                     ");
+        console.log("                                                     ", 
+        colors.reset);
 
         console.log("Entre com a opção desejada: ");
-        opcao = readlinesync.questionInt("");
+        opcao = Input.questionInt("");
 
-        if (opcao == 9) {
-            console.log("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+        if (opcao === 0) {
+            console.log(colors.fg.greenstrong, "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
             sobre();
+            console.log(colors.reset, "");
             process.exit(0);
         }
 
         switch (opcao) {
             case 1:
-                console.log("\n\nCriar Conta\n\n");
+                console.log(colors.fg.whitestrong, "\n\nCriar Conta\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 2:
-                console.log("\n\nListar todas as Contas\n\n");
+                console.log(colors.fg.whitestrong, "\n\nListar todas as Contas\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 3:
-                console.log("\n\nConsultar dados da Conta - por número\n\n");
+                console.log(colors.fg.whitestrong, "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 4:
-                console.log("\n\nAtualizar dados da Conta\n\n");
+                console.log(colors.fg.whitestrong, "\n\nAtualizar dados da Conta\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 5:
-                console.log("\n\nApagar uma Conta\n\n");
+                console.log(colors.fg.whitestrong, "\n\nApagar uma Conta\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 6:
-                console.log("\n\nSaque\n\n");
+                console.log(colors.fg.whitestrong, "\n\nSaque\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 7:
-                console.log("\n\nDepósito\n\n");
+                console.log(colors.fg.whitestrong, "\n\nDepósito\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 8:
-                console.log("\n\nTransferência entre Contas\n\n");
+                console.log(colors.fg.whitestrong, "\n\nTransferência entre Contas\n\n", colors.reset);
 
+                keyPress()
                 break;
             default:
-                console.log("\nOpção Inválida!\n");
+                console.log(colors.fg.whitestrong, "\nOpção Inválida!\n", colors.reset);
 
-                break;
+                keyPress()
         }
     }
- 
 
+}
 
 /* Função com os dados da pessoa desenvolvedora */
-
-export function sobre(): void {
+function sobre(): void {
     console.log("\n*****************************************************");
     console.log("Projeto Desenvolvido por: ");
-    console.log("Generation Brasil - generation@generation.org");
-    console.log("github.com/conteudoGeneration");
+    console.log("Matheus Tiago Canellas - mtcanellas@gmail.com");
+    console.log("github.com/mtcanellas");
     console.log("*****************************************************");
+}
+
+/* Função de pausa entre as opções do menu */
+function keyPress(): void {
+    console.log(colors.reset,"\nPressione enter para continuar...");
+    Input.prompt();
 }
 
 main();
--- src/model/Conta.ts
+++ src/model/Conta.ts
@@ -1,17 +1,15 @@
+import {colors } from "../util/Color";
+
 export class Conta{
 
-//atributos da Classe
+    // Atributos da Classe
+    private _numero: number;
+    private _agencia: number;
+    private _titular: string;
+    private _tipo: number;
+    private _saldo: number;
 
-private _numero: number;
-private _agencia: number;
-private _titular: string;
-private _tipo: number;
-private _saldo: number;
-
-
-
-//metodo construtor
-
+    // Método Construtor
 	constructor(numero: number, agencia: number, titular: string, tipo: number, saldo: number) {
 		this._numero = numero;
 		this._agencia = agencia;
@@ -19,93 +17,96 @@ private _saldo: number;
 		this._tipo = tipo;
 		this._saldo = saldo;
 	}
-
-    /**
-     * Getter numero
-     * @return {number}
-     */
+	
+    // Métodos Get e Set
 	public get numero(): number {
 		return this._numero;
 	}
 
-    /**
-     * Getter agencia
-     * @return {number}
-     */
 	public get agencia(): number {
 		return this._agencia;
 	}
 
-    /**
-     * Getter titular
-     * @return {string}
-     */
 	public get titular(): string {
 		return this._titular;
 	}
 
-    /**
-     * Getter tipo
-     * @return {number}
-     */
 	public get tipo(): number {
 		return this._tipo;
 	}
 
-    /**
-     * Getter saldo
-     * @return {number}
-     */
 	public get saldo(): number {
 		return this._saldo;
 	}
 
-    /**
-     * Setter numero
-     * @param {number} value
-     */
 	public set numero(value: number) {
 		this._numero = value;
 	}
 
-    /**
-     * Setter agencia
-     * @param {number} value
-     */
 	public set agencia(value: number) {
 		this._agencia = value;
 	}
 
-    /**
-     * Setter titular
-     * @param {string} value
-     */
 	public set titular(value: string) {
 		this._titular = value;
 	}
 
-    /**
-     * Setter tipo
-     * @param {number} value
-     */
 	public set tipo(value: number) {
 		this._tipo = value;
 	}
 
-    /**
-     * Setter saldo
-     * @param {number} value
-     */
 	public set saldo(value: number) {
 		this._saldo = value;
 	}
-	
 
-    //metodo auxiliares
+    //Métodos Auxiliares
 
-        public visualizar(): void{
-            console.log("**********************************");
-            console.log("              DADOS DA CONTA      ");
+    public sacar(valor: number): boolean {
+
+        if(valor <= 0){
+            console.log(colors.fg.red, "O valor deve ser positivo",colors.reset);
+            return false;
         }
 
+        if(valor > this._saldo){
+            console.log(colors.fg.red, "Saldo Insuficiente!",colors.reset);
+            return false;
+        }
+
+        this._saldo -= valor;
+        return true;
+    }
+
+    public depositar(valor: number): void {
+
+        if(valor <= 0)
+            console.log(colors.fg.red, "O valor deve ser positivo",colors.reset);
+        else
+            this._saldo += valor;
+    }
+
+    public visualizar(): void {
+
+        let tipo: string;
+
+        switch(this._tipo){
+            case 1:
+                tipo = "Conta Corrente";
+            break;
+            case 2:
+                tipo = "Conta Poupança";
+            break;
+            default:
+                tipo = "Tipo Inválido";
+        }
+
+        console.log("\n************************************");
+        console.log("        DADOS DA CONTA              ");
+        console.log("************************************");
+        console.log(`Número da conta: ${this._numero}`);
+        console.log(`Número da agência: ${this._agencia}`);
+        console.log(`Nome do titular: ${this._titular}`);
+        console.log(`Tipo da conta: ${tipo}`);
+        console.log(`Saldo da conta: R$ ${this._saldo.toFixed(2)}`);
+    }
 }
